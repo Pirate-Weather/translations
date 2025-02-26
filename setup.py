c18c6c17ee5fd9b4200c2b2a56adb7ee22bf350f
Kev <cloneofghosts@users.noreply.github.com>
2025-02-26 10:21:37 -0500

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 from setuptools import find_packages, setup
 
-__version__ = "0.0.8"
+__version__ = "1.0.0"
 
 
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")) as f:
